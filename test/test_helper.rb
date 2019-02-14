37cbe989a4360e0d4f26a404c42e28d41c0fbd42
Nick Sutterer <apotonick@gmail.com>
2019-02-14 20:48:58 +0100

    allow testing multiple End semantics.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,12 +22,29 @@ Minitest::Spec.class_eval do
 
   Linear = Trailblazer::Activity::DSL::Linear
 
-  def assert_process(seq, semantic, circuit)
+  def assert_process(seq, *args)
     process = compile_process(seq)
 
-    process.to_h[:outputs].inspect.must_equal %{[#<struct Trailblazer::Activity::Output signal=#<Trailblazer::Activity::End semantic=#{semantic.inspect}>, semantic=#{semantic.inspect}>]}
+    semantics, circuit = args[0..-2], args[-1]
+
+    inspects = semantics.collect { |semantic| %{#<struct Trailblazer::Activity::Output signal=#<Trailblazer::Activity::End semantic=#{semantic.inspect}>, semantic=#{semantic.inspect}>} }
+
+    process.to_h[:outputs].inspect.must_equal %{[#{inspects.join("")}]}
 
     cct = Cct(process: process)
     cct.must_equal %{#{circuit}}
+  end
+
+  Activity = Trailblazer::Activity
+
+  let(:implementing) do
+    implementing = Module.new do
+      extend T.def_tasks(:a, :b, :c, :d, :f, :g)
+    end
+    implementing::Start = Activity::Start.new(semantic: :default)
+    implementing::Failure = Activity::End(:failure)
+    implementing::Success = Activity::End(:success)
+
+    implementing
   end
 end
