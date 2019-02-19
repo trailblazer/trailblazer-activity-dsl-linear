ef2dbaa5dede6a644f614d62b61ed8cc288398d7
Nick Sutterer <apotonick@gmail.com>
2019-02-19 09:34:48 +0100

    prototype Activity as a class.

---
+++ test/activity_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class ActivityTest < Minitest::Spec
+  it "what" do
+    Class.new(Activity::Railway) do
+
+    end
+  end
+
+  # merge!
+  # inheritance
+  # :step_method
+  # macaroni
+end
