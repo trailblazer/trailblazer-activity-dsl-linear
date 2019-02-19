a92c68c996f08e882b3891cc79df6b22b5304f37
Nick Sutterer <apotonick@gmail.com>
2019-02-19 15:15:20 +0100

    test insertion options and third inheritance level with ACT.

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class ActivityTest < Minitest::Spec
-  it "allows inheritance" do
+  it "allows inheritance / INSERTION options" do
     implementing = self.implementing
 
     activity = Class.new(Activity::Path) do
@@ -12,6 +12,12 @@ class ActivityTest < Minitest::Spec
     sub_activity = Class.new(activity) do
       step implementing.method(:c), id: :c
       step implementing.method(:d), id: :d
+    end
+
+    sub_sub_activity = Class.new(sub_activity) do
+      step implementing.method(:g), id: :g, before: :b
+      step implementing.method(:f), id: :f, replace: :a
+      step nil                            , delete: :c
     end
 
     process = activity.to_h[:process]
@@ -36,6 +42,22 @@ class ActivityTest < Minitest::Spec
 <*#<Method: #<Module:0x>.b>>
  {Trailblazer::Activity::Right} => <*#<Method: #<Module:0x>.c>>
 <*#<Method: #<Module:0x>.c>>
+ {Trailblazer::Activity::Right} => <*#<Method: #<Module:0x>.d>>
+<*#<Method: #<Module:0x>.d>>
+ {Trailblazer::Activity::Right} => #<End/:success>
+#<End/:success>
+}
+
+    process = sub_sub_activity.to_h[:process]
+
+    assert_process_for process, :success, %{
+#<Start/:default>
+ {Trailblazer::Activity::Right} => <*#<Method: #<Module:0x>.f>>
+<*#<Method: #<Module:0x>.f>>
+ {Trailblazer::Activity::Right} => <*#<Method: #<Module:0x>.g>>
+<*#<Method: #<Module:0x>.g>>
+ {Trailblazer::Activity::Right} => <*#<Method: #<Module:0x>.b>>
+<*#<Method: #<Module:0x>.b>>
  {Trailblazer::Activity::Right} => <*#<Method: #<Module:0x>.d>>
 <*#<Method: #<Module:0x>.d>>
  {Trailblazer::Activity::Right} => #<End/:success>
