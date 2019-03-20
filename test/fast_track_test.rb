26ccce31ba96251af2cbae9d4de923f1f5b5ee63
Nick Sutterer <apotonick@gmail.com>
2019-03-20 14:08:14 +0100

    more tests for FT.

--- test/fast_track_test.rb
+++ test/fast_track_test.rb
@@ -106,6 +106,72 @@ class FastTrackTest < Minitest::Spec
       ctx.inspect.must_equal     %{{:seq=>[:f, :g, :c], :c=>Trailblazer::Activity::FastTrack::FailFast}}
 
     end
+
+    it "provides {:pass_fast} and {:fail_fast}" do
+      implementing = T.def_steps(:f, :a, :g, :c, :b, :d)
+
+      activity = Class.new(Activity::FastTrack) do
+        step implementing.method(:f), id: :f
+        fail implementing.method(:a), id: :a, fail_fast: true
+        step implementing.method(:g), id: :g, pass_fast: true, fail_fast: true
+        # step implementing.method(:c), id: :c, fail_fast: true, pass_fast: true
+        fail implementing.method(:b), id: :b
+        step implementing.method(:d), id: :d
+      end
+
+      process = activity.to_h
+
+      assert_process_for process, :success, :pass_fast, :fail_fast, :failure, %{
+#<Start/:default>
+ {Trailblazer::Activity::Right} => <*#<Method: #<Module:0x>.f>>
+<*#<Method: #<Module:0x>.f>>
+ {Trailblazer::Activity::Left} => <*#<Method: #<Module:0x>.a>>
+ {Trailblazer::Activity::Right} => <*#<Method: #<Module:0x>.g>>
+<*#<Method: #<Module:0x>.a>>
+ {Trailblazer::Activity::Left} => #<End/:fail_fast>
+ {Trailblazer::Activity::Right} => <*#<Method: #<Module:0x>.b>>
+<*#<Method: #<Module:0x>.g>>
+ {Trailblazer::Activity::Left} => #<End/:fail_fast>
+ {Trailblazer::Activity::Right} => #<End/:pass_fast>
+<*#<Method: #<Module:0x>.b>>
+ {Trailblazer::Activity::Left} => #<End/:failure>
+ {Trailblazer::Activity::Right} => #<End/:failure>
+<*#<Method: #<Module:0x>.d>>
+ {Trailblazer::Activity::Left} => #<End/:failure>
+ {Trailblazer::Activity::Right} => #<End/:success>
+#<End/:success>
+
+#<End/:pass_fast>
+
+#<End/:fail_fast>
+
+#<End/:failure>
+}
+
+  # g --> :pass_fast
+      signal, (ctx, _) = process.to_h[:circuit].([{seq: []}])
+
+      signal.inspect.must_equal  %{#<Trailblazer::Activity::End semantic=:pass_fast>}
+      ctx.inspect.must_equal     %{{:seq=>[:f, :g]}}
+
+  # a --> :failure
+      signal, (ctx, _) = process.to_h[:circuit].([{seq: [], f: false}])
+
+      signal.inspect.must_equal  %{#<Trailblazer::Activity::End semantic=:failure>}
+      ctx.inspect.must_equal     %{{:seq=>[:f, :a, :b], :f=>false}}
+
+  # a --> :fail_fast
+      signal, (ctx, _) = process.to_h[:circuit].([{seq: [], f: false, a: false}])
+
+      signal.inspect.must_equal  %{#<Trailblazer::Activity::End semantic=:fail_fast>}
+      ctx.inspect.must_equal     %{{:seq=>[:f, :a], :f=>false, :a=>false}}
+
+  # g --> :fail_fast
+      signal, (ctx, _) = process.to_h[:circuit].([{seq: [], g: false}])
+
+      signal.inspect.must_equal  %{#<Trailblazer::Activity::End semantic=:fail_fast>}
+      ctx.inspect.must_equal     %{{:seq=>[:f, :g], :g=>false}}
+    end
   end
 
 
