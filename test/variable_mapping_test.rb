032c2efbb439178760457ca08baa77b1d64633aa
Nick Sutterer <apotonick@gmail.com>
2019-03-22 11:43:39 +0100

    add I/O code, WIP.

---
+++ test/variable_mapping_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class IOTest < Minitest::Spec
+  it do
+    activity = Class.new(Trailblazer::Activity::Path) do
+
+    end
+  end
+end
+
+VariableMapping::Extension(
+Trailblazer::Activity::TaskWrap::Input.new(input),
+            Trailblazer::Activity::TaskWrap::Output.new(output))
+)
