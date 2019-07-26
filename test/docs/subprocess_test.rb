067f38ca02c2d9bec86259afacc26e2a70de4830
Nick Sutterer <apotonick@gmail.com>
2019-07-26 08:31:21 +0200

    add nested markers.

--- test/docs/subprocess_test.rb
+++ test/docs/subprocess_test.rb
@@ -5,20 +5,26 @@ class SubprocessTest < Minitest::Spec
     module A
       Memo = Class.new
 
+      #:nested
       class Memo::Validate < Trailblazer::Activity::Railway
         step :check_params
         step :check_attributes
-
+        #~methods
         include T.def_steps(:check_params, :check_attributes)
+        #~methods end
       end
+      #:nested end
 
+      #:container
       class Memo::Create < Trailblazer::Activity::Railway
         step :create_model
         step Subprocess(Memo::Validate)
         step :save
-
+        #~methods
         include T.def_steps(:create_model, :save)
+        #~methods
       end
+      #:container end
 
       signal, (ctx, _) = Memo::Create.([{seq: []}, {}])
       ctx.inspect.must_equal %{{:seq=>[:create_model, :check_params, :check_attributes, :save]}}
@@ -31,13 +37,16 @@ class SubprocessTest < Minitest::Spec
       Memo = Class.new
       Memo::Validate = A::Memo::Validate
 
+      #:reconnect
       class Memo::Create < Trailblazer::Activity::Railway
         step :create_model
         step Subprocess(Memo::Validate), Output(:failure) => Track(:success)
         step :save
-
+        #~methods
         include T.def_steps(:create_model, :save)
+        #~methods end
       end
+      #:reconnect end
 
       signal, (ctx, _) = Memo::Create.([{seq: []}, {}])
       ctx.inspect.must_equal %{{:seq=>[:create_model, :check_params, :check_attributes, :save]}}
@@ -51,20 +60,26 @@ class SubprocessTest < Minitest::Spec
     module C
       Memo = Class.new
 
+      #:end-nested
       class Memo::Validate < Trailblazer::Activity::Railway
         step :check_params, Output(:failure) => End(:invalid_params)
         step :check_attributes
-
+        #~methods
         include T.def_steps(:check_params, :check_attributes)
+        #~methods end
       end
+      #:end-nested end
 
+      #:end
       class Memo::Create < Trailblazer::Activity::Railway
         step :create_model
         step Subprocess(Memo::Validate), Output(:invalid_params) => Track(:failure)
         step :save
-
+        #~methods
         include T.def_steps(:create_model, :save)
+        #~methods end
       end
+      #:end end
 
       signal, (ctx, _) = Memo::Create.([{seq: []}, {}])
       ctx.inspect.must_equal %{{:seq=>[:create_model, :check_params, :check_attributes, :save]}}
