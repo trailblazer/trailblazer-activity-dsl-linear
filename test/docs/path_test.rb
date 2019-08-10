0f1d0e846921c1db91afc4dc8f2339fc702c6e47
Nick Sutterer <apotonick@gmail.com>
2019-08-10 12:39:51 +0200

    more tests for Path().

--- test/docs/path_test.rb
+++ test/docs/path_test.rb
@@ -38,9 +38,7 @@ class DocsPathTest < Minitest::Spec
 
     module B
       class Charge < Trailblazer::Activity::Path
-        extend T.def_tasks(:a, :b, :c, :d, :e)
-
-        out = self
+        include T.def_steps(:validate, :decide_type, :direct_debit, :finalize, :authorize, :charge)
 
         step :validate
         step :decide_type, Output(Trailblazer::Activity::Left, :credit_card) => Path(connect_to: Id(:finalize)) do
@@ -70,5 +68,13 @@ class DocsPathTest < Minitest::Spec
  {Trailblazer::Activity::Right} => #<End/:success>
 #<End/:success>
 }
+
+    ctx = {seq: []}
+
+    signal, (ctx, flow_options) = B::Charge.([ctx, {}])
+    ctx.inspect.must_equal %{{:seq=>[:validate, :decide_type, :direct_debit, :finalize]}}
+
+    signal, (ctx, flow_options) = B::Charge.([{seq: [], decide_type: false}, {}])
+    ctx.inspect.must_equal %{{:seq=>[:validate, :decide_type, :authorize, :charge, :finalize], :decide_type=>false}}
   end
 end
