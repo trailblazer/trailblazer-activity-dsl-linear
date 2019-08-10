10add0e23b62a5619490ab7e6036055f69583d90
Nick Sutterer <apotonick@gmail.com>
2019-08-10 15:24:49 +0200

    finish Path() docs.

--- test/docs/path_test.rb
+++ test/docs/path_test.rb
@@ -3,18 +3,19 @@ require "test_helper"
 class DocsPathTest < Minitest::Spec
   it do
     module A
+      #:path
       class Charge < Trailblazer::Activity::Path
-        extend T.def_tasks(:a, :b, :c, :d, :e)
-
-        out = self
-
+        #~meths
+        include T.def_tasks(:a, :b, :c, :d, :e)
+        #~meths end
         step :validate
-        step :decide_type, Output(Trailblazer::Activity::Left, :credit_card) => Path(end_id: "End.cc", end_task: End(:with_cc)) do
+        step :decide_type, Output(Activity::Left, :credit_card) => Path(end_id: "End.cc", end_task: End(:with_cc)) do
           step :authorize
           step :charge
         end
         step :direct_debit
       end
+      #:path end
     end
 
     assert_process_for A::Charge, :with_cc, :success, %{
@@ -37,9 +38,11 @@ class DocsPathTest < Minitest::Spec
 }
 
     module B
+      #:path-join
       class Charge < Trailblazer::Activity::Path
+        #~meths
         include T.def_steps(:validate, :decide_type, :direct_debit, :finalize, :authorize, :charge)
-
+        #~meths end
         step :validate
         step :decide_type, Output(Trailblazer::Activity::Left, :credit_card) => Path(connect_to: Id(:finalize)) do
           step :authorize
@@ -48,6 +51,7 @@ class DocsPathTest < Minitest::Spec
         step :direct_debit
         step :finalize
       end
+      #:path-join end
     end
 
     assert_process_for B::Charge, :success, %{
@@ -80,11 +84,26 @@ class DocsPathTest < Minitest::Spec
 
   it "works in Railway" do
     module C
+      CreditCard = Class.new
+      DebitCard  = Class.new
+
+      #:path-railway
       class Charge < Trailblazer::Activity::Railway
         MySignal = Class.new(Trailblazer::Activity::Signal)
-
+        #~meths
         include T.def_steps(:validate, :decide_type, :direct_debit, :finalize, :authorize, :charge)
-
+        #:path-decider
+        def decide_type(ctx, model:, **)
+          if model.is_a?(CreditCard)
+            return MySignal # go the Path() way!
+          elsif model.is_a?(DebitCard)
+            return true
+          else
+            return false
+          end
+        end
+        #:path-decider end
+        #~meths end
         step :validate
         step :decide_type, Output(MySignal, :credit_card) => Path(connect_to: Id(:finalize)) do
           step :authorize
@@ -93,6 +112,7 @@ class DocsPathTest < Minitest::Spec
         step :direct_debit
         step :finalize
       end
+      #:path-railway end
     end
 
     assert_process_for C::Charge, :success, :failure, %{
@@ -120,15 +140,13 @@ class DocsPathTest < Minitest::Spec
 #<End/:failure>
 }
 
-    ctx = {seq: []}
+    signal, (ctx, flow_options) = C::Charge.([{seq: [], model: C::DebitCard.new}, {}])
+    ctx.inspect.gsub(/0x\w+/, "").must_equal %{{:seq=>[:validate, :direct_debit, :finalize], :model=>#<DocsPathTest::C::DebitCard:>}}
 
-    signal, (ctx, flow_options) = C::Charge.([ctx, {}])
-    ctx.inspect.must_equal %{{:seq=>[:validate, :decide_type, :direct_debit, :finalize]}}
+    signal, (ctx, flow_options) = C::Charge.([{seq: [], model: C::CreditCard.new}, {}])
+    ctx.inspect.gsub(/0x\w+/, "").must_equal %{{:seq=>[:validate, :authorize, :charge, :finalize], :model=>#<DocsPathTest::C::CreditCard:>}}
 
-    signal, (ctx, flow_options) = C::Charge.([{seq: [], decide_type: false}, {}])
-    ctx.inspect.must_equal %{{:seq=>[:validate, :decide_type], :decide_type=>false}}
-
-    signal, (ctx, flow_options) = C::Charge.([{seq: [], decide_type: C::Charge::MySignal}, {}])
-    ctx.inspect.must_equal %{{:seq=>[:validate, :decide_type, :authorize, :charge, :finalize], :decide_type=>DocsPathTest::C::Charge::MySignal}}
+    signal, (ctx, flow_options) = C::Charge.([{seq: [], model: nil}, {}])
+    ctx.inspect.gsub(/0x\w+/, "").must_equal %{{:seq=>[:validate], :model=>nil}}
   end
 end
