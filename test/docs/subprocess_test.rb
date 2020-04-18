3d0c7139e09b515e72a1b5315881220686851e1c
Kamil Milewski <kamil.milewski@hotmail.com>
2020-04-18 11:09:21 +0200

    Add test for subprocess
    that makes sure Subprocess() does automatically wire all termini of a
    nested activity

--- test/docs/subprocess_test.rb
+++ test/docs/subprocess_test.rb
@@ -89,4 +89,40 @@ class SubprocessTest < Minitest::Spec
     end
   end
 
+  it "subprocess automatically wires all termini of a nested activity" do
+    module D
+      Memo = Class.new
+
+      #:end-nested
+      class Memo::JustPassFast < Trailblazer::Activity::FastTrack
+        step :just_pass_fast, pass_fast: true, fast_track: true
+        include T.def_steps(:just_pass_fast)
+        #~methods end
+      end
+      #:end-nested end
+
+      #:end
+      class Memo::Create < Trailblazer::Activity::FastTrack
+        step :create_model
+        step Subprocess(Memo::JustPassFast), fast_track: true
+        step :save
+        #~methods
+        include T.def_steps(:create_model, :save)
+        #~methods end
+      end
+      #:end end
+
+      # here we can see that failure, success, fail_fast and pass_fast has been wired
+      expected_wiring = "
+        SubprocessTest::D::Memo::JustPassFast
+          {#<Trailblazer::Activity::End semantic=:failure>} => #<End/:failure>
+          {#<Trailblazer::Activity::End semantic=:success>} => #<Trailblazer::Activity::TaskBuilder::Task user_proc=save>
+          {#<Trailblazer::Activity::End semantic=:fail_fast>} => #<End/:fail_fast>
+          {#<Trailblazer::Activity::End semantic=:pass_fast>} => #<End/:pass_fast>
+      ".gsub(/\s+/, "")
+
+      Trailblazer::Developer::Render::Circuit.(Memo::Create).gsub(/\s+/, "")
+        .must_include(expected_wiring)
+    end
+  end
 end
