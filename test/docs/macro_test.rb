0cf2b4c64bea595f460e5f09da73842c721a780c
Nick Sutterer <apotonick@gmail.com>
2020-08-27 11:19:29 +0200

    add a macro-test that demonstrates you can also use `Output` and friends in a macro.

--- test/docs/macro_test.rb
+++ test/docs/macro_test.rb
@@ -32,4 +32,63 @@ class DocsMacroTest < Minitest::Spec
 
     ctx.inspect.must_equal %{{:params=>{:user=>\"Adam\"}, :myparams=>{:user=>\"Adam\", :role=>\"sailor\"}}}
   end
+
+  it "what" do
+    module B
+      class User < Struct.new(:id)
+        def self.find_by(id:)
+          id && new(id)
+        end
+      end
+
+      #:output
+      module MyMacro
+        def self.FindModel(model_class)
+          # the inserted task.
+          task = ->((ctx, flow_options), _) do
+            model         = model_class.find_by(id: ctx[:params][:id])
+
+            return_signal = model ? Trailblazer::Activity::Right : Trailblazer::Activity::Left
+            ctx[:model]   = model
+
+            return return_signal, [ctx, flow_options]
+          end
+
+          # the configuration needed by Trailblazer's DSL.
+          {
+            task: task,
+            id:   :"find_model_#{model_class}",
+            Trailblazer::Activity::Railway.Output(:failure) => Trailblazer::Activity::Railway.End(:not_found)
+          }
+        end
+      end
+      #:output end
+
+      #:output-usage
+      class Create < Trailblazer::Activity::Railway
+        step MyMacro::FindModel(User)
+      end
+      #:output-usage end
+    end
+
+    signal, (ctx, _) = B::Create.([{params: {id: 1}}, {}])
+    ctx.inspect.must_equal %{{:params=>{:id=>1}, :model=>#<struct DocsMacroTest::B::User id=1>}}
+    signal.inspect.must_equal %{#<Trailblazer::Activity::End semantic=:success>}
+
+    signal, (ctx, _) = Trailblazer::Developer.wtf?(B::Create, [{params: {}}])
+    ctx.inspect.must_equal %{{:params=>{}, :model=>nil}}
+    signal.inspect.must_equal %{#<Trailblazer::Activity::End semantic=:not_found>}
+
+=begin
+#:output-result
+signal, (ctx, _) = Trailblazer::Developer.wtf?(User::Create, [{params: {id: nil}}])
+signal #=> #<Trailblazer::Activity::End semantic=:not_found>
+
+`-- User::Create
+    |-- Start.default
+    |-- find_model_User
+    `-- End.not_found
+#:output-result end
+=end
+  end
 end
