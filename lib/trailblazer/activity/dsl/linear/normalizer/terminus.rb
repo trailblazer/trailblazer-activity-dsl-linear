188b0892c50d487a8ef9358813203a6ed37b48be
Nick Sutterer <apotonick@gmail.com>
2022-04-13 09:53:18 +0200

    move Normalizer::Terminus to separate file.

---
+++ lib/trailblazer/activity/dsl/linear/normalizer/terminus.rb
@@ -0,0 +1,85 @@
+module Trailblazer
+  class Activity
+    module DSL
+      module Linear
+        module Normalizer
+          # Normalizer pipeline for the {terminus} DSL method.
+          module Terminus
+            module_function
+
+            def Normalizer
+              normalizer_steps =
+                {
+                "activity.normalize_step_interface"       => Normalizer.Task(Normalizer.method(:normalize_step_interface)),      # first
+                "activity.normalize_for_macro"            => Normalizer.Task(Normalizer.method(:merge_user_options)),
+                "activity.normalize_normalizer_options"   => Normalizer.Task(Normalizer.method(:merge_normalizer_options)),
+                "activity.normalize_non_symbol_options"   => Normalizer.Task(Normalizer.method(:normalize_non_symbol_options)),
+                "activity.normalize_context"              => Normalizer.method(:normalize_context),
+                "activity.normalize_id"                   => Normalizer.Task(Normalizer.method(:normalize_id)),
+                "terminus.normalize_task"          => Normalizer.Task(Terminus.method(:normalize_task)),
+                "terminus.normalize_magnetic_to"          => Normalizer.Task(Terminus.method(:normalize_magnetic_to)),
+                "terminus.append_end"                     => Normalizer.Task(Terminus.method(:append_end)),
+
+                "activity.compile_data" => Normalizer.Task(Normalizer.method(:compile_data)), # FIXME
+                "activity.create_row" => Normalizer.Task(Normalizer.method(:create_row)),
+                "activity.create_add" => Normalizer.Task(Normalizer.method(:create_add)),
+                "activity.create_adds" => Normalizer.Task(Normalizer.method(:create_adds)),
+                }
+
+              TaskWrap::Pipeline.new(normalizer_steps.to_a)
+            end
+
+            # @private
+            # Set {:task} and {:semantic}.
+            def normalize_task(ctx, task:, **)
+              if task.kind_of?(Activity::End) # DISCUSS: do we want this check?
+                ctx = _normalize_task_for_end_event(ctx, **ctx)
+              else
+                # When used such as {terminus :found}, create the end event automatically.
+                ctx = _normalize_task_for_symbol(ctx, **ctx)
+              end
+            end
+
+            def _normalize_task_for_end_event(ctx, task:, **) # you cannot override using {:semantic}
+              ctx.merge!(
+                semantic: task.to_h[:semantic]
+              )
+            end
+
+            def _normalize_task_for_symbol(ctx, task:, semantic: task, **)
+              ctx.merge!(
+                task:     Linear.End(semantic),
+                semantic: semantic,
+              )
+            end
+
+            # @private
+            def normalize_magnetic_to(ctx, magnetic_to: nil, semantic:, **)
+              return if magnetic_to
+              ctx.merge!(magnetic_to: semantic)
+            end
+
+            # @private
+            def append_end(ctx, task:, semantic:, append_to: "End.success", **)
+              terminus_args = {
+                sequence_insert: [Linear::Insert.method(:Append), append_to],
+                stop_event:      true
+              }
+
+              ctx.merge!(
+                {
+                  wirings:      [
+                    Linear::Search::Noop(
+                      Activity::Output.new(task, semantic), # DISCUSS: do we really want to transport the semantic "in" the object?
+                    )],
+                  adds: [],
+                  **terminus_args
+                }
+              )
+            end
+          end # Terminus
+        end
+      end
+    end
+  end
+end
