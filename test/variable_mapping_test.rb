88c939a8d072088276a559f5ab8d4eacd0e690b1
Nick Sutterer <apotonick@gmail.com>
2022-11-15 16:03:42 +0100

    use assert_invoke to remind me to use it everywhere. start unit tests for VariableMapping::SetVariable.

--- test/variable_mapping_test.rb
+++ test/variable_mapping_test.rb
@@ -24,12 +24,15 @@ class VariableMappingTest < Minitest::Spec
     end
     assert_match /missing keywords?: :?date/, exception.message
 
-  ## {:time} is passed-through.
-  ## {:date} is passed-through.
-  ## {:current_user} is defaulted through Inject()
+  #@ {:time} is passed-through.
+  #@ {:date} is passed-through.
+  #@ {:current_user} is defaulted through Inject()
+  #@ Injections are not visible outside.
   ## Note that Inject()s are put "on top" of the default input, no whitelisting is happening, we can still see {:model}.
-    signal, (ctx, _) = Trailblazer::Activity::TaskWrap.invoke(T::Create, [{time: "yesterday", date: "today", model: Object}, {}])
-    assert_equal ctx.inspect, '{:time=>"yesterday", :date=>"today", :model=>Object, :log=>"Called @ yesterday and \"today\" by [:time, :date, :model]{:time=>\"yesterday\", :date=>\"today\", :model=>Object}!", :private=>"[:time, :date, :model, :current_user, :log]"}'
+    assert_invoke T::Create, time: "yesterday", date: "today", model: Object, expected_ctx_variables: {
+      log: "Called @ yesterday and \"today\" by [:seq, :time, :date, :model]{:seq=>[], :time=>\"yesterday\", :date=>\"today\", :model=>Object}!",
+      private: "[:seq, :time, :date, :model, :current_user, :log]"
+    }
 
   ## {:time} is defaulted through kw
   ## {:current_user} is defaulted through Inject()
@@ -57,6 +60,38 @@ class VariableMappingTest < Minitest::Spec
         end
       end
     end
+
+  ## we can only see variables combined from Inject() and In() in the step.
+    signal, (ctx, _) = Trailblazer::Activity::TaskWrap.invoke(TT::Create, [{date: "today", model: Object, something: 99}, {}])
+    assert_equal ctx.inspect, '{:date=>"today", :model=>Object, :something=>99, :log=>"Called @ Time.now and \"today\" by [:date, :model, :something]!", :private=>"[:model, :thing, :date, :current_user, :log]Object"}'
+  end
+
+  it "Inject(circuit_interface: true)" do
+    module XX
+      class Create < Trailblazer::Activity::Railway
+        step :write,
+          Inject(circuit_interface: true) => {current_user: :my_instance_method},
+          # Inject() => [:date, :time],
+          # Inject() => {current_user: ->(ctx, **) { ctx.keys.inspect }},
+          In() => [:model],
+          In() => {:something => :thing}
+
+        def write(ctx, time: "Time.now", date:, current_user:, **) # {date} has no default configured.
+          ctx[:log] = "Called @ #{time} and #{date.inspect} by #{current_user}!"
+          ctx[:private] = ctx.keys.inspect
+          ctx[:private] += ctx[:model].inspect
+        end
+
+        def my_instance_method(ctx, model:, **)
+          {current_user: "<Currentuser for #{model}>", date: 1}
+        end
+      end
+    end # XX
+
+    assert_invoke XX::Create, time: "yesterday", date: "today", model: Object, expected_ctx_variables: {
+      log: "Called @ yesterday and \"today\" by [:seq, :time, :date, :model]{:seq=>[], :time=>\"yesterday\", :date=>\"today\", :model=>Object}!",
+      private: "[:seq, :time, :date, :model, :current_user, :log]"
+    }
 
   ## we can only see variables combined from Inject() and In() in the step.
     signal, (ctx, _) = Trailblazer::Activity::TaskWrap.invoke(TT::Create, [{date: "today", model: Object, something: 99}, {}])
@@ -695,5 +730,54 @@ class VariableMappingTest < Minitest::Spec
     signal, (ctx, flow_options) = Activity::TaskWrap.invoke(activity, [ctx, flow_options], **{})
 
     _(ctx.to_hash.inspect).must_equal %{{:a=>0, :b=>108, :model_a=>1, :model_b=>3, :model_add=>\"1\", :model_from_a=>1}}
+  end
+end
+
+class VariableMappingUnitTest < Minitest::Spec
+
+  describe "SetVariable" do
+    it "SetVariable#call can invoke a {Circuit.Step}" do
+      my_exec_context = Class.new do
+        def my_model(ctx, current_user:, **)
+          "<MyModel #{current_user}>"
+        end
+      end.new
+
+      user_filter = :my_model
+
+      filter = Trailblazer::Activity::Circuit.Step(user_filter, option: true)
+
+      pipe_task = Trailblazer::Activity::DSL::Linear::VariableMapping::SetVariable.new(:model, filter, user_filter)
+
+
+      ctx = {current_user: Object, mode: :update}
+
+
+      wrap_ctx = {aggregate: {}}
+
+
+      wrap_ctx, _ = pipe_task.(wrap_ctx, [[ctx, {}], {exec_context: my_exec_context}])
+
+      assert_equal wrap_ctx[:aggregate], {:model=>"<MyModel Object>"}
+    end
+
+    it "SetVariable#call can invoke any self-made circuit-step interface filter" do
+      my_lowlevel_inject_filter = ->((ctx, flow_options), **) { "<MyModel #{ctx.fetch(:current_user)}>" }
+
+      pipe_task = Trailblazer::Activity::DSL::Linear::VariableMapping::SetVariable.new(:model, my_lowlevel_inject_filter, my_lowlevel_inject_filter)
+
+
+      ctx = {current_user: Object, mode: :update}
+
+
+      wrap_ctx = {aggregate: {}}
+
+
+      wrap_ctx, _ = pipe_task.(wrap_ctx, [[ctx, {}], {}])
+
+      assert_equal wrap_ctx[:aggregate], {:model=>"<MyModel Object>"}
+      end
+
+
   end
 end
