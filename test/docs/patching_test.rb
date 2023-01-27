69fa724ad6c5a1e11d469ec92f95221925c37ef9
Nick Sutterer <apotonick@gmail.com>
2023-01-27 17:28:31 +0100

    better patching test.

--- test/docs/patching_test.rb
+++ test/docs/patching_test.rb
@@ -1,8 +1,9 @@
 require "test_helper"
 
-class DocsPatchingTest < Minitest::Spec
-  it do
-    module Memo
+#@ Test {Operation.patch}
+class PatchDSLTest < Minitest::Spec
+  module Song
+    module Operation
       #:delete_assets
       class DeleteAssets < Trailblazer::Activity::Railway
         step :rm_images
@@ -23,6 +24,51 @@ class DocsPatchingTest < Minitest::Spec
       end
       #:delete end
 
+      class Destroy < Trailblazer::Activity::Railway
+
+        #~meths
+        include T.def_steps(:policy, :find_model)
+        #~meths end
+
+        step :policy
+        step :find_model
+        step Subprocess(Delete), id: :delete
+      end
+    end # Operation
+  end
+
+  it "provides the {#patch} function" do
+    module Song::Operation
+      #:patch_function
+      class Erase < Destroy # we're inheriting from Song::Operation::Destroy
+        # def self.tidy_storage(ctx, **)
+        #   # delete files from your amazing cloud
+        #   true
+        # end
+        extend T.def_steps(:tidy_storage)
+
+        # step :policy
+        # step :find_model
+        # step Subprocess(Delete), id: "delete"
+        extend Trailblazer::Activity::DSL::Linear::Patch::DSL
+        patch(:delete, :delete_assets) {
+          step Erase.method(:tidy_storage), before: :rm_images
+        }
+      end
+      #:patch_function end
+    end
+
+    assert_invoke Song::Operation::Destroy, seq: %{[:policy, :find_model, :delete_model, :rm_images, :rm_uploads]}
+    assert_invoke Song::Operation::Erase, seq: %{[:policy, :find_model, :delete_model, :tidy_storage, :rm_images, :rm_uploads]}
+  end
+end
+
+#@ Test Subprocess(..., patch: ...)
+class DocsSubprocessPatchTest < Minitest::Spec
+  it do
+    Delete = PatchDSLTest::Song::Operation::Delete
+
+    module Memo
       #:destroy
       class Destroy < Trailblazer::Activity::Railway
         def self.tidy_storage(ctx, **)
@@ -44,18 +90,13 @@ class DocsPatchingTest < Minitest::Spec
       #:destroy end
     end
 
-    signal, (ctx, _) = Trailblazer::Developer.wtf?(Memo::Destroy, [{seq: []}])
-
-    _(ctx.inspect).must_equal %{{:seq=>[:policy, :find_model, :delete_model, :rm_images, :rm_uploads]}}
+    assert_invoke Memo::Destroy, seq: %{[:policy, :find_model, :delete_model, :rm_images, :rm_uploads]}
   end
 
   it do
-    module Asset
-      class Delete < Trailblazer::Activity::Railway
-        step :delete_model
-        include T.def_steps(:delete_model)
-      end
+    Delete = PatchDSLTest::Song::Operation::Delete
 
+    module Asset
       class Destroy < Trailblazer::Activity::Railway
         extend T.def_steps(:tidy_storage)
 
@@ -68,6 +109,9 @@ class DocsPatchingTest < Minitest::Spec
       end
     end
 
-    assert_invoke Asset::Destroy, seq: %{[:tidy_storage, :delete_model]}
+    assert_invoke Asset::Destroy, seq: %{[:tidy_storage, :delete_model, :rm_images, :rm_uploads]}
   end
 end
+
+
+
