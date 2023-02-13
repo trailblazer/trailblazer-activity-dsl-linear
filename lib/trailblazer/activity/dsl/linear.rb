ab742d7d829a8d5b5a110819685a6ae74227d7dc
Nick Sutterer <apotonick@gmail.com>
2023-02-13 10:45:29 +0100

    extract Wiring API code to normalizer/output_tuples.rb.

--- lib/trailblazer/activity/dsl/linear.rb
+++ lib/trailblazer/activity/dsl/linear.rb
@@ -35,6 +35,7 @@ require "trailblazer/activity/dsl/linear/sequence/search"
 require "trailblazer/activity/dsl/linear/sequence/compiler"
 require "trailblazer/activity/dsl/linear/normalizer/inherit" # DISCUSS. should we add normalizer/options/... or something?
 require "trailblazer/activity/dsl/linear/normalizer/extensions"
+require "trailblazer/activity/dsl/linear/normalizer/output_tuples"
 require "trailblazer/activity/dsl/linear/normalizer"
 require "trailblazer/activity/dsl/linear/normalizer/terminus"
 require "trailblazer/activity/dsl/linear/helper"
