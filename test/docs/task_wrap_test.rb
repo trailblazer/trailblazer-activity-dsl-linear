60acd812c7c810863ada094bfd912ff0e2b51600
Nick Sutterer <apotonick@gmail.com>
2023-06-15 17:16:16 +0200

    doc tests for taskWrap/runtime.

--- test/docs/task_wrap_test.rb
+++ test/docs/task_wrap_test.rb
@@ -86,9 +86,9 @@ class DocsRuntimeExtensionTest < Minitest::Spec
 
   module MyAPM # Advanced performance monitoring, done right!
     Store = []
-    Span = Struct.new(:name, :payload, :finish_data) do
-      def finish(data:)
-        self.finish_data = data
+    Span = Struct.new(:name, :payload, :finish_payload) do
+      def finish(payload:)
+        self.finish_payload = payload
       end
     end
 
@@ -98,6 +98,15 @@ class DocsRuntimeExtensionTest < Minitest::Spec
     end
   end
 
+  it "what" do
+    #:start
+    span = MyAPM.start_span("validate", payload: {time: Time.now})
+    # do whatever you have to...
+    span.finish(payload: {time: Time.now})
+    #:start end
+  end
+
+  #:myapm_start
   module MyAPM # Advanced performance monitoring, done right!
     module Extension
       def self.start_instrumentation(wrap_ctx, original_args)
@@ -116,49 +125,113 @@ class DocsRuntimeExtensionTest < Minitest::Spec
       end
     end
   end
+  #:myapm_start end
 
+  #:myapm
   module MyAPM # Advanced performance monitoring, done right!
     module Extension
       def self.finish_instrumentation(wrap_ctx, original_args)
         ctx   = original_args[0][0]
         span  = wrap_ctx[:span]
 
-        span.finish(data: ctx.inspect)
+        span.finish(payload: ctx.inspect)
 
         return wrap_ctx, original_args
       end
     end
   end
+  #:myapm end
 
   # APM, instrumentation
 
+  #:create
   module Song::Activity
     class Create < Trailblazer::Activity::Railway
-      step :model
+      step :create_model
       step :validate
-      step :save
+      left :handle_errors
+      step :notify
       #~meths
-      include T.def_steps(:model, :validate, :save)
+      include T.def_steps(:create_model, :validate, :notify)
       #~meths end
     end
   end
+  #:create end
 
-
-  it "runs new taskWrap steps" do
-    ext = Trailblazer::Activity::TaskWrap::Extension(
+  let(:apm_extension) do
+    #:runtime_ext
+    apm_extension = Trailblazer::Activity::TaskWrap::Extension(
       [MyAPM::Extension.method(:start_instrumentation),  id: "my_apm.start_span",  prepend: "task_wrap.call_task"],
       [MyAPM::Extension.method(:finish_instrumentation), id: "my_apm.finish_span", append: "task_wrap.call_task"],
     )
+    #:runtime_ext end
+  end
 
-    ctx = {song: {title: "Timebomb"}, seq: []}
+  it "runs apm steps around each step" do
+    MyAPM::Store = []
 
-    my_wrap = Hash.new(ext)
+    my_wrap = Hash.new(apm_extension)
 
-    signal, (ctx, _) = Song::Activity::Create.invoke([ctx, {}], wrap_runtime: my_wrap)
+    assert_invoke Song::Activity::Create,
+      circuit_options: {wrap_runtime: my_wrap},
+      song: {title: "Timebomb"},
+      seq: "[:create_model, :validate, :notify]"
 
-    assert_equal signal.to_h[:semantic], :success
-    assert_equal ctx.inspect, %{{:song=>{:title=>\"Timebomb\"}, :seq=>[:model, :validate, :save]}}
-    assert_equal MyAPM::Store.collect { |span| span.payload }.inspect, %{[{:id=>nil}, {:id=>\"Start.default\"}, {:id=>:model}, {:id=>:validate}, {:id=>:save}, {:id=>\"End.success\"}]}
+    assert_equal MyAPM::Store.collect { |span| span.payload }.inspect, %{[{:id=>nil}, {:id=>\"Start.default\"}, {:id=>:create_model}, {:id=>:validate}, {:id=>:notify}, {:id=>\"End.success\"}]}
+
+    #:runtime
+    my_wrap = Hash.new(apm_extension)
+
+    #:runtime_invoke
+    Song::Activity::Create.invoke(
+      [
+        # ctx:
+        {
+          song: {title: "Timebomb"},
+          #:meths
+          seq: []
+          #:meths end
+        }
+      ],
+      wrap_runtime: my_wrap # runtime taskWrap extensions!
+    )
+    #:runtime_invoke end
+    #:runtime end
+  end
+
+  it "runs apm only for {:validate}" do
+    #:runtime_validate
+    validate_task = Trailblazer::Activity::Introspect
+      .Nodes(Song::Activity::Create, id: :validate) # returns Node::Attributes
+      .task                                         # and the actually executed task from the circuit.
+
+    my_wrap = {validate_task => apm_extension}
+    #:runtime_validate end
+
+    MyAPM::Store = []
+
+    assert_invoke Song::Activity::Create,
+      circuit_options: {wrap_runtime: my_wrap},
+      song: {title: "Timebomb"},
+      seq: "[:create_model, :validate, :notify]"
+
+    assert_equal MyAPM::Store.collect { |span| span.payload }.inspect, %{[{:id=>:validate}]}
+
+  # the called activity itself is also taskWrapped!
+    #:wrap_create
+    my_wrap = {Song::Activity::Create => apm_extension}
+    #:wrap_create end
+
+    MyAPM::Store = []
+
+    assert_invoke Song::Activity::Create,
+      circuit_options: {wrap_runtime: my_wrap},
+      song: {title: "Timebomb"},
+      seq: "[:create_model, :validate, :notify]"
+
+    assert_equal MyAPM::Store.collect { |span| span.payload }.inspect, %{[{:id=>nil}]}
+
+    my_wrap = {Song::Activity::Create => apm_extension}
   end
 end
 
