9840df704e6aaf7ed6bea9d21173133ee5f9e62f
Nick Sutterer <apotonick@gmail.com>
2023-11-15 13:25:34 +0100

    move docs test to path_layout_test.rb.

---
+++ test/docs/path_layout_test.rb
@@ -0,0 +1,71 @@
+  it do
+    module A
+      Memo = DocsStrategyTest::Memo
+
+      #:step
+      module Memo::Activity
+        class Create < Trailblazer::Activity::Path
+          step :validate
+          step :create
+          #~mod
+          def validate(ctx, params:, **)
+            ctx[:input] = Form.validate(params)
+          end
+
+          def create(ctx, input:, **)
+            Memo.create(input)
+          end
+          #~mod end
+        end
+      end
+      #:step end
+    end
+
+    ctx = {params: {text: "Hydrate!"}}
+
+    signal, (ctx, flow_options) = A::Memo::Activity::Create.([ctx, {}])
+
+    _(signal.inspect).must_equal %{#<Trailblazer::Activity::End semantic=:success>}
+    _(ctx.inspect).must_equal %{{:params=>{:text=>\"Hydrate!\"}, :input=>{:text=>\"Hydrate!\"}}}
+  end
+
+  it do
+    module B
+      #:validate
+      class Create < Trailblazer::Activity::Path
+        #~flow
+        step :validate, Output(Trailblazer::Activity::Left, :failure) => End(:invalid)
+        step :create
+        #~flow end
+        #~mod
+        def validate(ctx, params:, **)
+          ctx[:input] = Form.validate(params) # true/false
+        end
+
+        def create(ctx, input:, **)
+          Memo.create(input)
+        end
+        #~mod end
+      end
+      #:validate end
+    end
+
+    ctx = {params: {text: "Hydrate!"}}
+    signal, (ctx, _flow_options) = B::Create.([ctx, {}])
+
+    _(signal.inspect).must_equal %{#<Trailblazer::Activity::End semantic=:success>}
+    _(ctx.inspect).must_equal %{{:params=>{:text=>\"Hydrate!\"}, :input=>{:text=>\"Hydrate!\"}}}
+
+    ctx = {params: nil}
+    signal, (ctx, _flow_options) = B::Create.([ctx, {}])
+    _(signal.inspect).must_equal %{#<Trailblazer::Activity::End semantic=:invalid>}
+    _(ctx.inspect).must_equal %{{:params=>nil, :input=>nil}}
+=begin
+    #:validate-call
+    ctx = {params: nil}
+    signal, (ctx, flow_options) = Memo::Create.([ctx, {}])
+
+    puts signal #=> #<Trailblazer::Activity::End semantic=:invalid>
+    #:validate-call end
+=end
+  end
