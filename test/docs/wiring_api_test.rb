300a6964b6ec86fb22dc5a204b05412ec501f12e
Nick Sutterer <apotonick@gmail.com>
2023-11-17 10:07:32 +0100

    more doc tests for Wiring API docs.

--- test/docs/wiring_api_test.rb
+++ test/docs/wiring_api_test.rb
@@ -1,5 +1,126 @@
 require "test_helper"
 
+#@ original Memo::Activity::Create
+class Vanilla_WiringApiDocsTest < Minitest::Spec
+  Memo = Class.new
+  module Memo::Activity
+    class Create < Trailblazer::Activity::Railway
+      step :validate
+      step :save
+      left :handle_errors
+      step :notify
+      #~meths
+      include T.def_steps(:validate, :save, :handle_errors, :notify)
+    end
+  end
+end
+
+#@ Output => End
+class Output_WiringApiDocsTest < Minitest::Spec
+  Memo = Class.new
+  module Memo::Activity
+    class Create < Trailblazer::Activity::Railway
+      step :validate
+      step :save,
+        Output(:failure) => End(:db_error)
+      left :handle_errors
+      step :notify
+      #~meths
+      include T.def_steps(:validate, :save, :handle_errors, :notify)
+    end
+  end
+
+  it "what" do
+    assert_invoke Memo::Activity::Create, seq: "[:validate, :save, :notify]"
+    assert_invoke Memo::Activity::Create, seq: "[:validate, :save]", save: false, terminus: :db_error
+  end
+end
+
+#@ Output => End
+class OutputOnLeft_WiringApiDocsTest < Minitest::Spec
+  Memo = Class.new
+  module Memo::Activity
+    class Create < Trailblazer::Activity::Railway
+      step :validate
+      step :save
+      left :fix_errors,
+        Output(:success) => Track(:success)
+      step :notify
+      #~meths
+      include T.def_steps(:validate, :save, :fix_errors, :notify)
+    end
+  end
+
+  it "what" do
+    assert_invoke Memo::Activity::Create, seq: "[:validate, :save, :notify]"
+    assert_invoke Memo::Activity::Create, seq: "[:validate, :save, :fix_errors, :notify]", save: false
+    assert_invoke Memo::Activity::Create, seq: "[:validate, :save, :fix_errors]", save: false, fix_errors: false, terminus: :failure
+  end
+end
+
+#@ Output => Track
+class OutputToSuccess_WiringApiDocsTest < Minitest::Spec
+  Memo = Class.new
+  #:output-track
+  module Memo::Activity
+    class Create < Trailblazer::Activity::Railway
+      step :validate
+      step :save,
+        Output(:failure) => Track(:success)
+      left :handle_errors
+      step :notify
+      #~meths
+      include T.def_steps(:validate, :save, :handle_errors, :notify)
+      #~meths end
+    end
+  end
+  #:output-track end
+
+  it "what" do
+    assert_invoke Memo::Activity::Create, seq: "[:validate, :save, :notify]"
+    assert_invoke Memo::Activity::Create, seq: "[:validate, :save, :notify]", save: false
+  end
+end
+
+#@ Output(semantic, Signal) => Track
+class ExplicitOutput_WiringApiDocsTest < Minitest::Spec
+  Memo = Struct.new(:save_result) do
+    def save; self.save_result;  end
+  end
+
+  #:output-explicit
+  module Memo::Activity
+    class Create < Trailblazer::Activity::Railway
+      class DbError < Trailblazer::Activity::Signal; end
+
+      step :validate
+      step :save,
+        Output(DbError, :database_error) => Track(:failure)
+      left :handle_errors
+      step :notify
+      #~meths
+      include T.def_steps(:validate, :handle_errors, :notify)
+      def save(ctx, model:, **)
+        #~code
+        database_broken = ctx[:database_broken]
+        #~code end
+        return DbError if database_broken
+
+        model.save
+      end
+      #~meths end
+    end
+  end
+  #:output-explicit end
+
+  it "what" do
+    assert_invoke Memo::Activity::Create, seq: "[:validate, :handle_errors]", validate: false, terminus: :failure
+    assert_invoke Memo::Activity::Create, seq: "[:validate, :notify]", model: Memo.new(true)
+    assert_invoke Memo::Activity::Create, seq: "[:validate, :handle_errors]", model: Memo.new(false), terminus: :failure
+    assert_invoke Memo::Activity::Create, seq: "[:validate, :handle_errors]", model: Memo.new(true), database_broken: true, terminus: :failure
+  end
+end
+
 class WiringApiDocsTest < Minitest::Spec
 # {#terminus} 1.0
   module A
