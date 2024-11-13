d0e47a0dcdc51a085b949493a4cfb5acf6db337d
Nick Sutterer <apotonick@gmail.com>
2024-11-13 10:18:13 +0100

    add more zeitwerk specifics.

--- lib/trailblazer/activity/dsl/linear/zeitwerk.rb
+++ lib/trailblazer/activity/dsl/linear/zeitwerk.rb
@@ -1,17 +1,34 @@
 module Trailblazer::Activity::DSL::Linear
   module Zeitwerk
     module Strategy
+      # DISCUSS: rename to {Finalize}?
+      # Don't recompile, just collect sequence steps.
       private def recompile_activity_for(type, *args, &block)
         sequence = apply_step_on_sequence_builder(type, *args, &block)
 
         @state.update!(:sequence) { |*| sequence }
       end
 
+      # Compile the final {Activity} via the sequence.
       def finalize!
         sequence = @state.get(:sequence)
-
+# puts "~~~~~~~~~ finalizing #{self.inspect}"
         recompile!(sequence)
       end
     end
-  end
+
+    module DSL
+      module Build
+        module_function
+        def Build(strategy, **options, &block)
+          activity_class = super
+puts "yoo"
+          activity_class.finalize!
+
+          activity_class
+        end
+      end
+
+    end
+  end # Zeitwerk
 end
