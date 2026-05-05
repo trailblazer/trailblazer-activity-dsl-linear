0ef021762ee9ce83def183a5e0166e5b7f5fce88
Nick Sutterer <apotonick@gmail.com>
2026-05-05 09:45:07 +0200

    fix

--- lib/trailblazer/activity/dsl/normalizer.rb
+++ lib/trailblazer/activity/dsl/normalizer.rb
@@ -7,7 +7,7 @@ module Trailblazer
 
           lib_ctx, _ = Circuit::Processor.(normalizer_pipe, {user_options: method_name, **options}, {}, nil, runner: Trailblazer::Circuit::Node::Runner)
 
-          return lib_ctx[:sequence_row]
+          return lib_ctx[:id], lib_ctx[:sequence_row]
         end
 
       end
