d08c63235ea677776d1faeee022920b837f11e72
Nick Sutterer <apotonick@gmail.com>
2026-09-01 10:27:51 +0200

    Path() now works absolutely lovely!

--- lib/trailblazer/activity/dsl/feature/path.rb
+++ lib/trailblazer/activity/dsl/feature/path.rb
@@ -15,129 +15,56 @@ module Trailblazer
           end
 
           # FIXME: we're defauting adds_for_sequence, which is redundant, see DSL::Normalizer::Step.
-          def call(block_arg: -> {}, adds_for_sequence: [], **ctx) # This is called in OutputTuples::Normalizer#
-            build_path(block_arg: block_arg, **options)
+          def call(block_arg: -> {}, adds_for_sequence: [], sequence:, **ctx) # This is called in OutputTuples::Normalizer#
+            build_path(block_arg: block_arg, sequence: sequence, **options)
           end
 
-          def build_path(track_color: "track_#{rand}", connect_to: nil, before: false, block_arg:, terminus: nil, exec_context:, **options)
-            options =
-              if connect_to
-                {}
-              elsif terminus
-                options.merge(
-                  end_task: Activity::Path.send(:Terminus, terminus),
-                  # end_id:   "End.#{terminus}"
-                )
-              else # Path() with End() inside block.
-                options
-                # FIXME: should we remove this?
-              end
-
+          # TODO: deprecate {:terminus} etc.
+          def build_path(track_name: "track_#{rand}", connect_to:, block_arg:, exec_context:, **options)
             # DISCUSS:  if anyone overrides `#step` in the "outer" activity, this won't be applied inside the branch.
-            activity, builder = Activity.Path(exec_context: exec_context, &block_arg)
+            _block_arg = -> {
+              extend Trailblazer::Activity::DSL::Topology::Helper
+              instance_exec(&block_arg)
+            }
+            activity, builder = Activity.Path(track_name: track_name, exec_context: exec_context, &_block_arg)
 
             seq = builder.sequence
             seq = seq.to_a.to_h # FIXME: introduce canonical way to work on Sequence.
 
-
-            # Strip default ends `Start.default` and `End.success` (if present).
+            # Strip default terminus `End.success` (if present).
             seq = seq.reject { |id, _| id == :"End.success" }
-            pp seq
+
+            # Rewire the last step.
+            last_step_id = seq.keys.last
+            last_step = seq[last_step_id]
+
+            adds_for_path = []
 
             if connect_to
-              seq = connect_for_sequence(seq, connect_to: connect_to)
+              new_wirings = last_step.wirings.collect do |output, target|
+                search, adds_from_target = connect_to.(**options)
+
+                adds_for_path += adds_from_target
+
+                [output, search]
+              end.to_h
+
+              adds_for_path = adds_for_path.collect { |(id, *row)| [id, [id, *row]] }.to_h
+              adds_for_path = adds_for_path.values # filter out double entries eg. Terminus(:received) for two outputs.
+
+              last_step.wirings = new_wirings # DISCUSS: mutability?
+              seq[last_step_id] = last_step
             end
 
             # Add the path elements before {End.success}.
             # Termini (or :stop_event) are to be placed after {End.success}.
-            adds_for_path = seq.collect do |id, row|
-              # the terminus of the path goes _after_ {End.success} into the "end group".
-              # insert_method = row.data[:stop_event] ? :append : :prepend
-              insert_method = :after
-
-              insert_target = :"End.success" # insert before/after
-         # insert_target = before if before && connect_to.instance_of?(Linear::Normalizer::OutputTuples::Track) # FIXME: this is a bit hacky, of course!
-
-              # ADDS friendly interface:
-              [
-                id, row, insert_method, insert_target
-              ]
+            adds_for_path += seq.collect do |id, row|
+              [id, row, :before, :"End.success"]
             end
 
             # Connect the Output() => Track(path_track)
-            # Linear::Normalizer::OutputTuples::Track.new(track_color, adds, {})
-
-            return OutputTuples::Target::Track.new(track_color, adds_for_path, {}).()
+            return OutputTuples::Target::Track.new(track_name, adds_for_path , {}).()
           end
-
-          # # module Path
-          # #   # Normalizer steps to handle Path() macro.
-          # #   module Normalizer
-          # #     module_function
-
-          # #     # Replace a block-expecting {PathBranch} instance with another one that's holding
-          # #     # the global {:block} from {#step ... do end}.
-          # #     def forward_block_for_path_branch(ctx, flow_options, _, block:, normalizer_options:, **)
-          # #       return ctx, flow_options unless block
-
-          # #       output, path_branch =
-          # #         ctx.find { |output, cfg| cfg.is_a?(Linear::PathBranch) }
-
-          # #       path_branch_with_block = Linear::PathBranch.new(
-          # #         normalizer_options
-          # #           .merge(path_branch.options)
-          # #           .merge(block: block)
-          # #       )
-
-          # #       ctx = ctx.merge(
-          # #         output => path_branch_with_block
-          # #       )
-
-          # #       return ctx, flow_options
-          # #     end
-
-          #     # Convert all occurrences of Path() to a corresponding {Track}.
-          #     # The {Track} instance contains all additional {adds} steps and
-          #     # is picked up in {Normalizer.normalize_connections_from_dsl}.
-          #     def convert_paths_to_tracks(ctx, flow_options, _, block: false, **)
-          #       new_tracks = ctx
-          #         .find_all { |output, cfg| cfg.is_a?(Linear::PathBranch) }
-          #         .collect {  |output, cfg| [output, Path.convert_path_to_adds(block: block, **cfg.options)]  }
-          #         .to_h
-
-          #       ctx = ctx.merge(new_tracks)
-
-          #       return ctx, flow_options
-          #     end
-          #   end # Normalizer
-
-
-
-          # Connect last row of the {sequence} to the given step via its {Id}
-          # Useful when steps needs to be inserted in between {Start} and {connect Id()}.
-          private def connect_for_sequence(sequence, connect_to:)
-            termini = sequence.find_all { |row| row.data[:stop_event] }
-            user_steps = sequence - termini
-
-            last_step_on_path = user_steps[-1]
-            output_2_search   = last_step_on_path[2]
-# pp last_step_on_path
-
-            # we want to reconnect the last step's {:success} output, everything else we keep.
-            success_output,  _ = output_2_search  .find { |output, _| output.to_h[:semantic] == :success }
-            raise if success_output.nil?
-
-              # FIXME: what about End()?
-            success_search = Sequence::Search::ById.new(connect_to.value) if connect_to.instance_of?(Linear::Normalizer::OutputTuples::Id)
-            success_search = Sequence::Search::Forward.new(connect_to.color) if connect_to.instance_of?(Linear::Normalizer::OutputTuples::Track) # FIXME: use existing mapping logic!
-
-            output_2_search[success_output] = success_search # replace the success search strategy.
-
-            row_options = last_step_on_path.to_h.merge(wirings: output_2_search)
-            row = Sequence.Row(**row_options)
-
-            user_steps[0..-2] + [row] + termini
-          end # Path
         end
       end
     end
--- test/helper/path_helper_test.rb
+++ test/helper/path_helper_test.rb
@@ -3,10 +3,10 @@ require "test_helper"
 class PathHelperTest < Minitest::Spec
   # it "{Path()} connects to {End.failure} when no {:terminus} given" do
 
-  it "Path(terminus: :success)" do
+  it "Path(connect_to: Track(:success))" do
     my_activity = Class.new(Trailblazer::Activity::Railway) do
       step :a
-      step :b, Output(:failure) => Path(terminus: :success) do
+      step :b, Output(:failure) => Path(connect_to: Track(:success)) do
         step :c
         step :d
       end
@@ -17,6 +17,116 @@ class PathHelperTest < Minitest::Spec
     end
 
     assert_run my_activity, terminus: :success, seq: [:a, :b, :e]
-    assert_run my_activity, terminus: :success, seq: [:a, :b, :c, :d], target_ctx: {seq: [1], b: false}
+    assert_run my_activity, terminus: :success, seq: [1, :a, :b, :c, :d, :e], target_ctx: {seq: [1], b: false}
+    # Within the Path, there's only a "success" track.
+    assert_run my_activity, terminus: :success, seq: [1, :a, :b, :c, :d, :e], target_ctx: {seq: [1], b: false, c: false}
+    assert_run my_activity, terminus: :success, seq: [1, :a, :b, :c, :d, :e], target_ctx: {seq: [1], b: false, d: false}
+    assert_run my_activity, terminus: :failure, seq: [1, :a, :b, :c, :d, :e, :f], target_ctx: {seq: [1], b:false, e: false}
+    assert_run my_activity, terminus: :failure, seq: [1, :a, :b, :e, :f], target_ctx: {seq: [1], e: false}
+  end
+
+  it "Path(connect_to: Id(:g))" do
+    my_activity = Class.new(Trailblazer::Activity::Railway) do
+      step :a
+      step :b, Output(:failure) => Path(connect_to: Id(:g)) do
+        step :c
+        step :d
+      end
+      step :e
+      step :g
+      left :f
+      step :h
+
+      include T.def_steps(:a, :b, :c, :d, :e, :f, :g, :h)
+    end
+
+    assert_run my_activity, terminus: :success, seq: [:a, :b, :e, :g, :h]
+    assert_run my_activity, terminus: :success, seq: [1, :a, :b, :c, :d, :g, :h], target_ctx: {seq: [1], b: false}
+    # Within the Path, there's only a "success" track, we never reach {:e} from the Path().
+    assert_run my_activity, terminus: :success, seq: [1, :a, :b, :c, :d, :g, :h], target_ctx: {seq: [1], b: false, c: false}
+    assert_run my_activity, terminus: :success, seq: [1, :a, :b, :c, :d, :g, :h], target_ctx: {seq: [1], b: false, d: false}
+    assert_run my_activity, terminus: :failure, seq: [1, :a, :b, :e, :f], target_ctx: {seq: [1], e: false}
+    assert_run my_activity, terminus: :failure, seq: [1, :a, :b, :e, :g, :f], target_ctx: {seq: [1], g: false}
+    assert_run my_activity, terminus: :failure, seq: [1, :a, :b, :e, :g, :h], target_ctx: {seq: [1], h: false}
+  end
+
+  it "Path(connect_to: Terminus(:success))" do
+    my_activity = Class.new(Trailblazer::Activity::Railway) do
+      step :a
+      step :b, Output(:failure) => Path(connect_to: Terminus(:success)) do
+        step :c
+        step :d
+      end
+      step :e
+      left :f
+
+      include T.def_steps(:a, :b, :c, :d, :e, :f)
+    end
+
+    assert_run my_activity, terminus: :success, seq: [:a, :b, :e]
+    assert_run my_activity, terminus: :success, seq: [1, :a, :b, :c, :d], target_ctx: {seq: [1], b: false}
+    assert_run my_activity, terminus: :success, seq: [1, :a, :b, :c, :d], target_ctx: {seq: [1], b: false, c: false}
+    assert_run my_activity, terminus: :success, seq: [1, :a, :b, :c, :d], target_ctx: {seq: [1], b: false, d: false}
+    assert_run my_activity, terminus: :failure, seq: [1, :a, :b, :e, :f], target_ctx: {seq: [1], e: false}
+  end
+
+  it "Path(connect_to: Terminus(:failure))" do
+    my_activity = Class.new(Trailblazer::Activity::Railway) do
+      step :a
+      step :b, Output(:failure) => Path(connect_to: Terminus(:failure)) do
+        step :c
+        step :d
+      end
+      step :e
+      left :f
+
+      include T.def_steps(:a, :b, :c, :d, :e, :f)
+    end
+
+    assert_run my_activity, terminus: :success, seq: [:a, :b, :e]
+    assert_run my_activity, terminus: :failure, seq: [1, :a, :b, :c, :d], target_ctx: {seq: [1], b: false}
+    assert_run my_activity, terminus: :failure, seq: [1, :a, :b, :c, :d], target_ctx: {seq: [1], b: false, c: false}
+    assert_run my_activity, terminus: :failure, seq: [1, :a, :b, :c, :d], target_ctx: {seq: [1], b: false, d: false}
+    assert_run my_activity, terminus: :failure, seq: [1, :a, :b, :e, :f], target_ctx: {seq: [1], e: false}
+  end
+
+  it "Path(connect_to: Terminus(:received)), where {:received} is a new terminus" do
+    my_activity = Class.new(Trailblazer::Activity::Railway) do
+      step :a
+      step :b, Output(:failure) => Path(connect_to: Terminus(:received)) do
+        step :c
+        step :d
+      end
+      step :e
+      left :f
+
+      include T.def_steps(:a, :b, :c, :d, :e, :f)
+    end
+
+    assert_run my_activity, terminus: :success, seq: [:a, :b, :e]
+    assert_run my_activity, terminus: :received, seq: [1, :a, :b, :c, :d], target_ctx: {seq: [1], b: false}
+    assert_run my_activity, terminus: :received, seq: [1, :a, :b, :c, :d], target_ctx: {seq: [1], b: false, c: false}
+    assert_run my_activity, terminus: :received, seq: [1, :a, :b, :c, :d], target_ctx: {seq: [1], b: false, d: false}
+    assert_run my_activity, terminus: :failure, seq: [1, :a, :b, :e, :f], target_ctx: {seq: [1], e: false}
+  end
+
+  it "Path(connect_to: false)" do
+    my_activity = Class.new(Trailblazer::Activity::Railway) do
+      step :a
+      step :b, Output(:failure) => Path(connect_to: false) do
+        step :c
+        step :d, Output(:success) => Terminus(:received), Output(:failure) => Track(:failure)
+      end
+      step :e
+      left :f
+
+      include T.def_steps(:a, :b, :c, :d, :e, :f)
+    end
+
+    assert_run my_activity, terminus: :success, seq: [:a, :b, :e]
+    assert_run my_activity, terminus: :received, seq: [1, :a, :b, :c, :d], target_ctx: {seq: [1], b: false}
+    assert_run my_activity, terminus: :received, seq: [1, :a, :b, :c, :d], target_ctx: {seq: [1], b: false, c: false}
+    assert_run my_activity, terminus: :failure, seq: [1, :a, :b, :c, :d, :f], target_ctx: {seq: [1], b: false, d: false}
+    assert_run my_activity, terminus: :failure, seq: [1, :a, :b, :e, :f], target_ctx: {seq: [1], e: false}
   end
 end
