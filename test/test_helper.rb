adb0c91080608f9762b8aacfc489f95186af98ad
Nick Sutterer <apotonick@gmail.com>
2026-09-05 10:41:15 +0200

    use DSL method instead of our own test mock logic.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,8 +23,8 @@ end
 module MyTest
   MySuccess = Trailblazer::Activity::Terminus::Success.new(semantic: :success)
 
-  def self.wirings_for_terminus(signal: Trailblazer::Activity::Right)
-    {Trailblazer::Activity::Output.new(signal, :success) => Trailblazer::Activity::DSL::Sequence::Search::Nil.new()}
+  def self.wirings_for_terminus
+    Trailblazer::Activity::DSL.wirings_for_terminus(signal: Trailblazer::Activity::Right, semantic: :success)
   end
 
   def self.options_for_mock_terminus(task: MySuccess, semantic: :success)
